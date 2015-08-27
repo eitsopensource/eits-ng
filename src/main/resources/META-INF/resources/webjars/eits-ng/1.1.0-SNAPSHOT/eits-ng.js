3230e952d1b5695751bcbb0dc4c30361fa7cb61d
rodrigo <rodrigo@eits.com.br>
2015-08-27 14:19:44 -0300

    adicionado a primeira versao

---
+++ src/main/resources/META-INF/resources/webjars/eits-ng/1.1.0-SNAPSHOT/eits-ng.js
@@ -0,0 +1,14 @@
+(function() {
+	'use strict';
+
+	/**
+	 * @ngdoc module
+	 * @name material.components.list
+	 * @description List module
+	 */
+	angular.module('eits-ng', [ 
+	    'eits-dwrBroker',
+		'eits.angularTranslate',
+	]);
+
+})();
